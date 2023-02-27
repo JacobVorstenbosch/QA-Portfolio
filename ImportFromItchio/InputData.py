e73303be134ce79d19855e9eaac7c1fb54566839
Jacob <jacobdvorstenbosch@gmail.com>
2023-02-27 16:31:46 -0500

    Updated script to 4.3 Selenium

--- ImportFromItchio/InputData.py
+++ ImportFromItchio/InputData.py
@@ -1,10 +1,11 @@
+#Updated for Selenium's 4.3 update (removal of 'find_element_by')
+
 import gspread, time
 import csv
 from oauth2client.service_account import ServiceAccountCredentials
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
 
 #much of this is commented out due to test attempts that I tried. Each attempt was useful in certain scenarios but it completely was dependent on when and where. 
 #Left it all to show exactly what I was doing and when.
@@ -23,13 +24,13 @@ driver.get('https://itch.io/my-purchases/bundles')
 
 #username and password blanked out because I don't want that info going out there.
 def login(driver):
-    user_name = driver.find_element_by_name('username')
-    password = driver.find_element_by_name('password')
+    user_name = driver.find_element("name", "username")
+    password = driver.find_element("name", "password")
 
     user_name.send_keys('')
-    password.send_keys('')
+    password.send_keys('Vampiric$4')
 
-    login_button = driver.find_element_by_class_name('button')
+    login_button = driver.find_element("")
     login_button.click()
 
 login(driver)
@@ -40,39 +41,41 @@ with open('read_data.csv') as csv_file:
     for csv_row in file_reader:
         csv_data.append(csv_row)
 
-bundle_link = driver.find_element_by_link_text('Bundle for Racial Justice and Equality')
+#bundle_link = driver.find_element("link text", "Bundle for Racial Justice and Equality")
+bundle_link = driver.find_element("link text", "Bundle for Ukraine")
 bundle_link.click()
 
-desired_page = 60
-finished_page = 30
+#set the desired page - usually the last one - to know when to have the program stop.
+desired_page = 35
 current_page = 1
-row = driver.find_elements_by_class_name('game_row')
+row = driver.find_elements("class name", "game_row")
 
 game_list = [["Game Name", "Game Author", "Game URL"]]
 
-#to do - fix game_row - i think we need to make a list for game_row?
-#the for loop is broken, and fixing it will make everythig ez
-
-'''
+#finds the elements necessary, stores them in a list and then adds them to the sheet that was defined above.
+#timer can be adjusted to your needs, but do remember that if it isn't long enough the data may not get parsed in time.
 while current_page < desired_page:
-    row = driver.find_elements_by_class_name('game_row')
+    row = driver.find_elements("class name", "game_row")
     for rows in row:
-        games = rows.find_element_by_class_name('game_title')
-        games_creator = rows.find_element_by_class_name('game_author').text
-        game_links = rows.find_element_by_tag_name('a')
+        games = rows.find_element("class name", "game_title")
+        games_creator = rows.find_element("class name", "game_author").text
+        game_links = rows.find_element("tag name", "a")
         uncoded_game_link = game_links.get_attribute('href')
         final_game_link = str(uncoded_game_link)
 
         game_list = [current_page, games.text, games_creator[3:], final_game_link]
         sheet.append_row(game_list)
 
-    next_page = driver.find_element_by_class_name('next_page')
+    next_page = driver.find_element("class name", "next_page")
     next_page.click()
     current_page+=1
     time.sleep(30)
-'''
+
+#only activate below if you want a horrifying list of nonsense.
+
 #print(game_list)
 
+#the for loop below is the initial attempt and does not work.
 '''
 for linker in games:
     game_links = linker.find_elements_by_tag_name('a')
@@ -83,13 +86,15 @@ for linker in games:
 '''
 
 #add encode if things go south
+#Initial way of doing things to print to CSV, due to its location in here it only prints the first 30.
+#could alter it to work, but printing to a google sheet is more efficient.
 with open('list.csv', 'w', newline='', encoding='UTF8') as file:
     writer = csv.writer(file)
-    row = driver.find_elements_by_class_name('game_row')
+    row = driver.find_elements("class name", "game_row")
     for rows in row:
-        games1 = rows.find_element_by_class_name('game_title')
-        games_creator1 = rows.find_element_by_class_name('game_author').text
-        game_links1 = rows.find_element_by_tag_name('a')
+        games1 = rows.find_element("class name", "game_title")
+        games_creator1 = rows.find_element("class name", "game_author").text
+        game_links1 = rows.find_element("tag name", "a")
         uncoded_game_links = game_links1.get_attribute('href')
         final_game_links = str(uncoded_game_links)
         writer.writerow((current_page, games1.text, games_creator1[3:], final_game_links))   
@@ -100,7 +105,7 @@ from string import Template
 '''
      
 '''
-
+#jfiddle is broken as of 2022, this no longer works and can be considered deprecated until fixed.
 chart_data = [["Bundle Item", "Number In Bundle"]]
 csv_data = chart_data
 
@@ -142,6 +147,3 @@ for row in chart_data[1:]:
 completedHtml = htmlString.substitute(labels=csv_data[0], data=chart_data_str)
 with open('Chart.html','w') as f:
  f.write(completedHtml)
-
-#Learned New Way O:
-#Need to figure out what elements are needed for the for loop
